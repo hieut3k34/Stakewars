d15103cb8e9d48634ba3718488b381c3b8bc79f4
hieut3k34 <phanhieuk11@gmail.com>
2022-08-30 16:37:52 +0700

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,36 @@
+DATE=$(date +%Y-%m-%d-%H-%M)
+DATADIR=/root/.near
+BACKUPDIR=BackupNear/near_${DATE}
+
+sudo mkdir $BACKUPDIR
+
+sudo systemctl stop neard
+
+wait
+
+echo "NEAR node was stopped" | ts
+
+if [ -d "$BACKUPDIR" ]; then
+    echo " Delete Old File " | ts
+        find BackupNear -type f -mtime 0.1 -exec rm -rf {} \;
+    echo " Delete Done " | ts
+    echo "Backup started" | ts
+    cp -rf  $DATADIR/data/  ${BACKUPDIR}/
+
+    tar -czvf ${BACKUPDIR}.tar.gz  ${BACKUPDIR}
+    rm -rf ${BACKUPDIR}
+
+    # Submit backup completion status, you can use healthchecks.io, betteruptime.com or other services
+    # Example
+    # curl -fsS -m 10 --retry 5 -o /dev/null https://hc-ping.com/xXXXxXXx-XxXx-XXXX-XXXx-...
+
+    echo "Backup completed" | ts
+else
+    echo $BACKUPDIR is not created. Check your permissions.
+    exit 0
+fi
+
+sudo systemctl start neard
+
+echo "NEAR node was started" | ts
+
