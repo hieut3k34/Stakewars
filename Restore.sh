a7815434babda742ab684dbe5c422308088db0fd
hieut3k34 <phanhieuk11@gmail.com>
2022-08-31 10:06:36 +0700

    Update Restore.sh

--- Restore.sh
+++ Restore.sh
@@ -9,7 +9,7 @@ echo "Restore started" | ts
 tar -xzvf $NAME.tar.gz
 rm -rf /root/.near/data
 cp -rf $NAME/data   /root/.near/
-
+curl -fsS -m 10 --retry 5 -o /dev/null  https://hc-ping.com/79903c6a-0367-4b4d-9ad0-4687ea830563
 echo "Restore completed" | ts
 else
  echo $NAME is not found
